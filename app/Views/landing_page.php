21d4591b9f459e8335b4faa6420d1a1b090747dd
Rivanza16 <rivanza.syifa21@students.unila.ac.id>
2023-11-07 10:23:27 +0700

    fix auth

--- app/Views/landing_page.php
+++ app/Views/landing_page.php
@@ -32,7 +32,7 @@
                         <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                         <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
                         <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
-                        <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                        <li class="nav-item"><a class="nav-link" href="login">Login</a></li>
                     </ul>
                 </div>
             </div>
