99a0364705b075eb85077cd153cc6cd2cd6676c0
amalianurulrahmawati <amelianurulrahmawati@gmail.com>
2023-11-20 19:42:52 +0700

    update profile pasien

--- app/Views/update_profile.php
+++ app/Views/update_profile.php
@@ -71,7 +71,62 @@
 <?php elseif(in_groups('pasien')): ?>
 <?= $this->extend('layouts/app');?>
 <?= $this->section('content') ?>
-<!-- Isinya -->
+<div class="col-12">
+			<!-- Page title -->
+			<div class="my-3 mx-3">
+				<h3>My Profile</h3>
+				<hr>
+			</div>
+			<!-- Form START -->
+            <form action="<?=base_url('/pasien/profile/edit/'.user_id())?>" method="POST"  enctype="multipart/form-data">
+            <?= csrf_field() ?>
+			<input type="hidden" name="_method" value="PUT">
+				<div class="row mb-1 gx-5">
+					<!-- Contact detail -->
+					<div class="col-xxl-8 mb-5 mb-xxl-0">
+						<div class="bg-secondary-soft px-4 py-4 rounded">
+							<div class="row g-3">
+								<h4 class="mb-2 mt-0">Detail Kontak</h4>
+								<!-- Nama -->
+								<div class="col-md-6">
+									<label class="form-label">Nama *</label>
+									<input type="text" class="form-control" name="nama_pasien" aria-label="Nama" value="<?= $pasien->nama_pasien;?>">
+								</div>
+								<!-- Username -->
+								<div class="col-md-6">
+									<label class="form-label">Username *</label>
+									<input type="text" class="form-control" name="username" aria-label="Username" value="<?=$pasien->username;?>">
+								</div>
+								<!-- Tanggal Lahir -->
+								<div class="col-md-6">
+									<label class="form-label">Tanggal Lahir *</label>
+									<input type="date" class="form-control" name="tanggal_lahir" aria-label="Tanggal Lahir" value="<?=$pasien->tanggal_lahir;?>">
+								</div>
+								<!-- Alamat -->
+								<div class="col-md-6">
+									<label class="form-label">Alamat *</label>
+									<input type="text" class="form-control" name="alamat_pasien" aria-label="Alamat" value="<?=$pasien->alamat_pasien;?>">
+								</div>
+								<!-- Nomor Kontak -->
+								<div class="col-md-6">
+									<label class="form-label">Nomor Kontak *</label>
+									<input type="text" class="form-control" name="nomor_kontak" aria-label="Nomor Kontak" value="<?=$pasien->nomor_kontak;?>">
+								</div>
+								<!-- Email -->
+								<div class="col-md-6">
+									<label class="form-label">Email *</label>
+									<input type="text" class="form-control" name="email"  value="<?=$pasien->email;?>">
+								</div>
+								<div class="col-md-8 mt-3">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                                <a href="<?=base_url('/pasien/profile')?>" class="btn btn-primary">Back</a>
+								</div>
+							</div> <!-- Row END -->
+                        </div>    
+                    </div>   
+                </div>    
+                </form> 
+            </div>
 
 <?=$this->endSection()?>
 <?php endif; ?>
